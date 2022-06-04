ba45daf4ad5d64cda3f058f959dcf040423123c9
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-06-04 09:49:19 +0300

    deep freeze elf store while not in production

--- apps/ntuadt/src/main.ts
+++ apps/ntuadt/src/main.ts
@@ -5,10 +5,17 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import { devTools } from '@ngneat/elf-devtools';
+import { elfHooks, deepFreeze } from '@ngneat/elf';
+
 devTools();
 
 if (environment.production) {
   enableProdMode();
+}
+if (!environment.production) {
+  elfHooks.registerPreStoreUpdate((currentState, nextState, storeName) => {
+    return deepFreeze(nextState);
+  });
 }
 
 platformBrowserDynamic()
