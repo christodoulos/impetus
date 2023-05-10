02d1e39c9f0f53a09e718ce245f131abc6c8bb0b
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-05-10 20:25:06 +0300

    larger json payloads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Set the limit for JSON payloads
+  app.use(bodyParser.json({ limit: '50mb' }));
+  // Set the limit for URL-encoded payloads
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
