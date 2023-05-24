3a9de3439469c2b7fec90fbccbeec9624547387d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-05-24 11:44:41 +0300

    backend @port 3335

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,13 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   // Set the limit for URL-encoded payloads
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  const globalPrefix = 'v2/api';
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors({ origin: 'http://localhost:4200', preflightContinue: true });
-  const port = process.env.PORT || 3334;
+  app.enableCors({
+    origin: ['http://localhost:4200', 'https://beta.atticadt.uwmh.eu'],
+    preflightContinue: true,
+  });
+  const port = process.env.PORT || 3335;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
